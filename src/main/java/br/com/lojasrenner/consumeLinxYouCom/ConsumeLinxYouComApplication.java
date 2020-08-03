ea8ca3813b0805381ffeed7095ce3ec4a1c91941
LucasBarboza <lucas.barboza19@gmail.com>
2020-08-03 10:03:44 -0300

    Adição versinoamento com o git

---
+++ src/main/java/br/com/lojasrenner/consumeLinxYouCom/ConsumeLinxYouComApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojasrenner.consumeLinxYouCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeLinxYouComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeLinxYouComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lojasrenner/estruturante/importLinxOperations/ImportLinxOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojasrenner.estruturante.importLinxOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportLinxOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
